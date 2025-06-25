2d414e2606d460f8f6ee96a51a64f5ee4b3bd908
dipto <iamdipto7@gmail.com>
2025-06-25 10:46:19 +0900

    remove html formatter

--- nvim-kickstart/.config/nvim/lua/plugins/conform.lua
+++ nvim-kickstart/.config/nvim/lua/plugins/conform.lua
@@ -10,8 +10,8 @@ return {
             -- Conform will run the first available formatter
             javascript = { "prettierd", "prettier", stop_after_first = true },
             typescript = { "prettierd", "prettier", stop_after_first = true },
-            css = { "prettierd", "prettier", stop_after_first = true },
-            html = { "prettierd", "prettier", stop_after_first = true },
+            -- css = { "prettierd", "prettier", stop_after_first = true },
+            -- html = { "prettierd", "prettier", stop_after_first = true },
             json = { "prettierd", "prettier", stop_after_first = true },
         },
         format_on_save = {
