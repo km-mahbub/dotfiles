63470f03d492c13a6dc349b3a8c7095ff584bfda
dipto <iamdipto7@gmail.com>
2025-06-26 00:55:25 +0900

    enable typescript inlay hints

--- nvim-kickstart/.config/nvim/lua/plugins/typescript-tools.lua
+++ nvim-kickstart/.config/nvim/lua/plugins/typescript-tools.lua
@@ -4,31 +4,13 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {
         settings = {
-            typescript = {
-                inlayHints = {
-                    -- You can set this to 'all' or 'literals' to enable more hints
-                    includeInlayParameterNameHints = "none", -- 'none' | 'literals' | 'all'
-                    includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                    includeInlayFunctionParameterTypeHints = false,
-                    includeInlayVariableTypeHints = false,
-                    includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-                    includeInlayPropertyDeclarationTypeHints = false,
-                    includeInlayFunctionLikeReturnTypeHints = true,
-                    includeInlayEnumMemberValueHints = true,
-                },
-            },
-            javascript = {
-                inlayHints = {
-                    -- You can set this to 'all' or 'literals' to enable more hints
-                    includeInlayParameterNameHints = "none", -- 'none' | 'literals' | 'all'
-                    includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                    includeInlayVariableTypeHints = false,
-                    includeInlayFunctionParameterTypeHints = false,
-                    includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-                    includeInlayPropertyDeclarationTypeHints = false,
-                    includeInlayFunctionLikeReturnTypeHints = true,
-                    includeInlayEnumMemberValueHints = true,
-                },
+            tsserver_file_preferences = {
+                includeInlayParameterNameHints = "all",
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
             },
         },
     },
