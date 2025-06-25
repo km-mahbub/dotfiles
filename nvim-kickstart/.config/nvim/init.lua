e05c885c1e080b7a3115e65562b472b509fd3df6
dipto <iamdipto7@gmail.com>
2025-06-25 18:29:06 +0900

    add terminal pop

--- nvim-kickstart/.config/nvim/init.lua
+++ nvim-kickstart/.config/nvim/init.lua
@@ -1,2 +1,3 @@
 require("config.lazy")
 require("current-theme")
+require("terminalpop")
--- nvim-kickstart/.config/nvim/lua/plugins/typescript-tools.lua
+++ nvim-kickstart/.config/nvim/lua/plugins/typescript-tools.lua
@@ -1,6 +1,22 @@
 return {
     "pmizio/typescript-tools.nvim",
-    enabled = false,
+    enabled = true,
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
+    opts = {
+        settings = {
+            -- All these are optional and can be customized
+            expose_as_code_action = "all",
+            tsserver_plugins = {},
+            -- Inlay hints config
+            inlay_hints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            },
+        },
+    },
 }
